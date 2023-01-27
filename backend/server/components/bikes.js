2d0a2c670aa4e5846c8de51bcb8440a62bb7adac
Lazar Narcis <lnarcis310@gmail.com>
2023-01-28 01:06:51 +0200

    fix bug at register date -> bikes

--- backend/server/components/bikes.js
+++ backend/server/components/bikes.js
@@ -6,6 +6,31 @@ bikes.get("/getBikes", (req, res) => {
     database.query("SELECT description, extra_information, price_per_minute, register_date, type, bikes.id as bike_id, bike_types.id as type_id FROM bikes LEFT JOIN bike_types ON bike_types.id=bikes.type", (err, result, fields) => {
         if (err) throw err;
         if (result.length != 0) {
+            for (let i = 0; i < result.length; i++) {
+                let register_date = new Date(result[i]['register_date']);
+                let year = register_date.getFullYear();
+                let month = register_date.getMonth() + 1;
+                let dt = register_date.getDate();
+                let hr = register_date.getHours();
+                let mn = register_date.getMinutes();
+                let sc = register_date.getSeconds();
+                if (dt < 10) {
+                    dt = '0' + dt;
+                }
+                if (month < 10) {
+                    month = '0' + month;
+                }
+                if (hr < 10) {
+                    hr = '0' + hr;
+                }
+                if (mn < 10) {
+                    mn = '0' + mn;
+                }
+                if (sc < 10) {
+                    sc = '0' + sc;
+                }
+                result[i]['register_date'] = year + '-' + month + '-' + dt + " " + hr + ":" + mn + ":" + sc;
+            }
             res.json({ message: result });
         } else {
             res.json({ message: `No bikes!`});
@@ -14,23 +39,44 @@ bikes.get("/getBikes", (req, res) => {
 });
 
 bikes.get("/getBike", (req, res) => {
-    const extra_information = req.query.extra_information;
-    database.query(`SELECT description, extra_information, price_per_minute, register_date, type, bikes.id as bike_id, bike_types.id as type_id FROM bikes LEFT JOIN bike_types ON bike_types.id=bikes.type WHERE extra_information LIKE "%${extra_information}%"`, (err, result, fields) => {
+    const bike_id = req.query.bike_id;
+    database.query(`SELECT description, extra_information, price_per_minute, register_date, type, bikes.id as bike_id, bike_types.id as type_id FROM bikes LEFT JOIN bike_types ON bike_types.id=bikes.type WHERE bikes.id="${bike_id}"`, (err, result, fields) => {
         if (err) throw err;
         if (result.length != 0) {
+            let register_date = new Date(result[0]['register_date']);
+            let year = register_date.getFullYear();
+            let month = register_date.getMonth() + 1;
+            let dt = register_date.getDate();
+            let hr = register_date.getHours();
+            let mn = register_date.getMinutes();
+            let sc = register_date.getSeconds();
+            if (dt < 10) {
+                dt = '0' + dt;
+            }
+            if (month < 10) {
+                month = '0' + month;
+            }
+            if (hr < 10) {
+                hr = '0' + hr;
+            }
+            if (mn < 10) {
+                mn = '0' + mn;
+            }
+            if (sc < 10) {
+                sc = '0' + sc;
+            }
+            result[0]['register_date'] = year + '-' + month + '-' + dt + " " + hr + ":" + mn + ":" + sc;
             res.json({ message: result });
         } else {
-            res.json({ message: `No bikes with this name! (${extra_information})`});
+            res.json({ message: `No bike with ID ${bike_id}!`});
         }
     });
 });
 
 bikes.get("/addBike", (req, res) => {
     const extra_information = req.query.extra_information;
-    const d = new Date();
-    const register_date = d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate();
     const type = req.query.type;
-    database.query(`INSERT INTO bikes (type, register_date, extra_information) VALUES ("${type}", "${register_date}", "${extra_information}")`, (err, result, fields) => {
+    database.query(`INSERT INTO bikes (type, extra_information) VALUES ("${type}", "${extra_information}")`, (err, result, fields) => {
         if (err) throw err;
         let new_bike_id = result.insertId;
         res.json({ message: `Success! A new bike with ID ${new_bike_id} has been added!` });
@@ -44,7 +90,10 @@ bikes.get("/deleteBike", (req, res) => {
         if (result.length) {
             database.query(`DELETE FROM bikes WHERE id="${bike_id}"`, (err, result, fields) => {
                 if (err) throw err;
-                res.json({ message: `Bike with ID ${bike_id} has been deleted!` });
+                database.query(`DELETE FROM rentals WHERE bike_id="${bike_id}"`, (err, result, fields) => {
+                    if (err) throw err;
+                    res.json({ message: `Bike with ID ${bike_id} has been deleted!` });
+                });
             });
         } else {
             res.json({ message: `The bike with ID ${bike_id} does not exist in database!` });
--- backend/server/database.js
+++ backend/server/database.js
@@ -2,13 +2,13 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-let mysql_data = {
+let data = {
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.NAME
 };
-const connection = mysql.createConnection(mysql_data);
+const connection = mysql.createConnection(data);
 connection.connect();
 
 module.exports = connection;
