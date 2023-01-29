f6b84874e11cdf2556d4eecd9fe0dc744ddcefb9
Alex.Ghighe <ghighe88@gmail.com>
2023-01-29 22:09:50 +0200

    create folder utils and paste generateAlert there

--- frontend/src/Components/Bikes/AddBikeForm.js
+++ frontend/src/Components/Bikes/AddBikeForm.js
@@ -1,46 +1,68 @@
 import axios from "axios";
-import { useRef } from "react";
-import { generateAlert } from "../../functions";
+import { useState } from "react";
+import generateAlert from "../../utils/generateAlert";
+
+let initialFormInputs = {
+  id: "",
+  description: "",
+  price_per_minute: ""
+};
 
 const AddBikeForm = () => {
-  const getBikeTypeInput = useRef();
-  const extraInformationInput = useRef();
-  const getPriceInput = useRef();
-  const formReset = useRef();
+  const [formInputs, setFormInputs] = useState(initialFormInputs);
+
+  let isFormValid = false;
+
+  if (
+    formInputs.id &&
+    formInputs.price_per_minute &&
+    formInputs.description.length > 5
+  ) {
+    isFormValid = true;
+  }
+
+  const idChangeHandler = (e) => {
+    setFormInputs({ ...formInputs, id: e.target.value });
+  };
+
+  const priceChangeHandler = (e) => {
+    setFormInputs({ ...formInputs, price_per_minute: e.target.value });
+  };
+
+  const infoChangeHandler = (e) => {
+    setFormInputs({ ...formInputs, description: e.target.value });
+  };
 
   function addBikeType(event) {
     event.preventDefault();
-    const bikeTypeValue = getBikeTypeInput.current.value;
-    const priceValue = getPriceInput.current.value;
-    const informationValue = extraInformationInput.current.value;
 
     const data = {
-      id: bikeTypeValue,
-      description: informationValue,
-      price_per_minute: +priceValue
+      id: formInputs.id,
+      description: formInputs.description,
+      price_per_minute: +formInputs.price_per_minute
     };
-
-    formReset.current.reset();
-
     axios.post("/bike_types/addBikeType", data).then((response) => {
       generateAlert(response.data.type, response.data.message);
     });
+    setFormInputs(initialFormInputs);
+    isFormValid = false;
   }
 
   return (
-    <div className="flex justify-center bg-white rounded-lg border border-gray-300 py-40 max-w-[30%] mx-[10%] text-lg font-medium shadow-lg relative">
-      <div className="absolute top-0 text-center text-white w-full py-4 overflow-hidden bg-dark-red">
+    <div className=" flex justify-center mt-10 bg-white rounded-lg border border-gray-300 py-40 text-sm font-sm shadow-lg relative md:p-20">
+      <div className="absolute top-0  w-full py-4 bg-dark-red text-center text-white">
         Add Bike Types
         <span className="block text-sm text-center">
           Add new bike type in the system
         </span>
       </div>
-      <form className="w-full max-w-md" onSubmit={addBikeType} ref={formReset}>
+      <form className="w-full max-w-md" onSubmit={addBikeType}>
         <div className=" border-b border-dark-red py-2">
           <input
             className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 ml-2  py-1 px-2 leading-tight focus:outline-none"
             type="text"
-            ref={getBikeTypeInput}
+            value={formInputs.id}
+            onChange={idChangeHandler}
             placeholder="Type(1-classic,2-electric,3-scooter...)"
             aria-label="Type(1-classic,2-electric,3-scooter...)"
           />
@@ -50,7 +72,8 @@ const AddBikeForm = () => {
           <input
             className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 ml-2  py-1 px-2 leading-tight focus:outline-none"
             type="number"
-            ref={getPriceInput}
+            value={formInputs.price_per_minute}
+            onChange={priceChangeHandler}
             placeholder="Price per minute.."
             aria-label="Price per minute.."
           />
@@ -78,16 +101,20 @@ const AddBikeForm = () => {
               id="exampleFormControlTextarea1"
               rows="3"
               placeholder="Here you can specify additional information"
-              ref={extraInformationInput}
+              value={formInputs.description}
+              onChange={infoChangeHandler}
             ></textarea>
           </div>
         </div>
-        <button
-          type="submit"
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border rounded"
-        >
-          Add Entry
-        </button>
+        <div className="flex justify-center">
+          <button
+            type="submit"
+            disabled={!isFormValid}
+            className=" bg-red-500 hover:bg-red-700 text-white font-bold focus:outline-none py-2 px-4 border rounded disabled:opacity-75 disabled:cursor-not-allowed"
+          >
+            Add Entry
+          </button>
+        </div>
       </form>
     </div>
   );
--- frontend/src/functions.js
+++
@@ -1,7 +0,0 @@
-import Swal from "sweetalert2";
-
-function generateAlert(type, message) {
-    return Swal.fire((type === "error" ? "Error!" : "Success!"), `${message}`, (type === "error" ? "warning" : "success"));
-}
-
-export { generateAlert };
