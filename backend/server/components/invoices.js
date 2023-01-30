c19bf35dd76dbc621209e9f38afa7fc29a03c4c8
Lazar Narcis <lnarcis310@gmail.com>
2023-01-30 23:51:02 +0200

    add revenue data

--- backend/server/components/invoices.js
+++ backend/server/components/invoices.js
@@ -3,7 +3,7 @@ const invoices = express.Router();
 const database = require("../database");
 
 invoices.get("/getInvoices", (req, res) => {
-    database.query("SELECT * FROM invoices", (err, result, fields) => {
+    database.query("SELECT * FROM invoice", (err, result, fields) => {
         if (err) {
             res.json({ type: "error", message: err.sqlMessage + ". Query: " + err.sql });
             return;
