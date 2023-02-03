6bd4e56e61d3bb65f461cd99bf15b9a08b8d2eeb
Lazar Narcis <lnarcis310@gmail.com>
2023-02-03 23:57:04 +0200

    add edit bike form

--- frontend/src/Components/Bikes/EditBikeForm.js
+++ frontend/src/Components/Bikes/EditBikeForm.js
@@ -1,57 +1,141 @@
-import { MdDirectionsBike, MdOutlineFormatListNumbered } from "react-icons/md";
-import fetchData from "../../utils/fetchEndPoints";
 import { useEffect, useState } from "react";
 import generateAlert from "../../utils/generateAlert";
+import fetchData from "../../utils/fetchEndPoints";
 
-const EditBikeForm = ({ addCount }) => {
-  const [bike_types, setBikeTypes] = useState([]);
-  const [message, setMessage] = useState({});
+const url = ["/bike_types/editBikeType", "/bike_types/getBikeType"];
 
-  const deleteBikeType = async (id) => {
-    const response = await fetchData("/bike_types/deleteBikeType", "POST", {
-      id
-    });
-    setMessage(response);
+const AddBikeForm = ({ setAddCount, showInput }) => {
+  let initialFormInputs = {
+    id: showInput,
+    description: "",
+    price_per_minute: ""
   };
+  const [formInputs, setFormInputs] = useState(initialFormInputs);
 
-  const editBikeType = (id) => {};
+  let isFormValid = false;
 
   useEffect(() => {
-    if (message.type) {
-      generateAlert(message.type, message.message);
-    }
-
+    const data = {
+        id: showInput
+    };
     (async () => {
-      const response = await fetchData("/bike_types/getBikeTypes");
-      setBikeTypes(response.message);
-      console.log("executed useEffect editbike");
+        const response = await fetchData(url[1], "POST", data);
+        if (response.type !== "error") {
+            setFormInputs({
+                id: response.message[0].id,
+                description: response.message[0].description,
+                price_per_minute: response.message[0].price_per_minute 
+            });
+        }
     })();
-  }, [message, addCount]);
+  }, [showInput]);
+
+  if (
+    formInputs.id &&
+    formInputs.price_per_minute &&
+    formInputs.description.length > 5
+  ) {
+    isFormValid = true;
+  }
+
+  const idChangeHandler = (e) => {
+    setFormInputs({ ...formInputs, id: e.target.value });
+  };
+
+  const priceChangeHandler = (e) => {
+    setFormInputs({ ...formInputs, price_per_minute: e.target.value });
+  };
+
+  const infoChangeHandler = (e) => {
+    setFormInputs({ ...formInputs, description: e.target.value });
+  };
+
+  function editBikeType(event) {
+    event.preventDefault();
+
+    const data = {
+      id: formInputs.id,
+      description: formInputs.description,
+      price_per_minute: +formInputs.price_per_minute
+    };
+    (async () => {
+      const response = await fetchData(url[0], "POST", data);
+      if (response.type !== "error") {
+        setFormInputs(initialFormInputs);
+        setAddCount((currCount) => currCount + 1);
+      }
+      generateAlert(response.type, response.message);
+    })();
+    isFormValid = false;
+  }
 
   return (
-    <div className="flex justify-center w-[80%] mt-10 mx-auto bg-white rounded-lg border border-gray-300 py-40 text-sm font-sm shadow-lg relative md:p-20">
-      <div className="absolute flex font-bold cursor-pointer top-0 w-full py-4 bg-dark-red text-white">
-        Modify: <MdOutlineFormatListNumbered className="mx-4" />{" "}
-        <span className="active:translate-y-1">Bike Types</span>
-        <MdDirectionsBike className="mx-4" />
-        <span className="active:translate-y-1">Bikes</span>
-      </div>
-      <div className="flex flex-col justify-between text-left mr-96">
-        {Array.isArray(bike_types) &&
-          bike_types.map((item, key) => {
-            return (
-              <div key={key} className="mt-8 text-sm">
-                {item.id}. {item.description} ({item.price_per_minute}$ per
-                minute){" "}
-                <button onClick={() => editBikeType(item.id)}>✏ </button>
-                <button onClick={() => deleteBikeType(item.id)}>X</button>
-                <hr className="bg-gray-800 mt-2"></hr>
-              </div>
-            );
-          })}
-      </div>
-    </div>
+    <>
+      <form
+        className="w-full max-w-md flex flex-col justify-center items-center"
+        onSubmit={editBikeType}
+      >
+        <div className=" border-b border-gray-300 py-2 ">
+          <input
+            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 ml-2  py-1 px-2 leading-tight focus:outline-none focus:shadow-outline border-b border-dark-red-500"
+            type="text"
+            value={formInputs.id}
+            disabled={true}
+            onChange={idChangeHandler}
+            placeholder="Type(1-classic,2-electric,3-scooter...)"
+            aria-label="Type(1-classic,2-electric,3-scooter...)"
+          />
+        </div>
+
+        <div className=" border-b border-gray-300 py-2 mt-2">
+          <input
+            className="bg-transparent border-none w-full text-gray-700 mr-3 ml-2  py-1 px-2 leading-tight focus:outline-none appearance-none"
+            type="number"
+            value={formInputs.price_per_minute}
+            onChange={priceChangeHandler}
+            placeholder="Price per minute.."
+            aria-label="Price per minute.."
+          />
+        </div>
+        <div className="mb-3 xl:w-96">
+          <label
+            htmlFor="exampleFormControlTextarea1"
+            className="form-label inline-block mb-2 text-gray-400 mt-1 relative top-2"
+          >
+            Extra
+          </label>
+          <textarea
+            className="
+
+        border
+        w-full
+        px-3
+        py-1.5
+        focus:border-dark-red
+        appearance-none
+        outline-0
+        resize-none
+      "
+            id="exampleFormControlTextarea1"
+            rows="3"
+            placeholder="Here you can specify additional information"
+            value={formInputs.description}
+            onChange={infoChangeHandler}
+          ></textarea>
+        </div>
+
+        <div className="flex justify-center">
+          <button
+            type="submit"
+            disabled={!isFormValid}
+            className=" bg-red-500 hover:bg-red-700 text-white font-bold focus:outline-none py-2 px-4 border rounded disabled:opacity-75 disabled:cursor-not-allowed"
+          >
+            Edit bike type
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
-export default EditBikeForm;
+export default AddBikeForm;
--- frontend/src/Pages/Bikes.js
+++ frontend/src/Pages/Bikes.js
@@ -1,21 +1,38 @@
 import AddBikeForm from "../Components/Bikes/AddBikeForm";
+import ShowBikeTypes from "../Components/Bikes/ShowBikeTypes";
 import EditBikeForm from "../Components/Bikes/EditBikeForm";
 import { useState } from "react";
 const Bikes = () => {
   const [addCount, setAddCount] = useState(0);
+  const [showAddBikes, setShowAddBikes] = useState(false);
+  const [showInput, setShowInput] = useState("");
+  const [isAnimating, setIsAnimating] = useState(false);
 
   return (
     <>
-      <div className="flex justify-center w-1/2 mt-10 m-auto bg-white rounded-lg border border-gray-300 py-40 text-sm font-sm shadow-lg relative md:p-20">
+      <div
+        style={{display: showAddBikes ? "none" : ""}}
+        className="flex justify-center w-1/2 mt-10 m-auto bg-white rounded-lg border border-gray-300 py-40 text-sm font-sm shadow-lg relative md:p-20">
         <div className="absolute top-0  w-full py-4 bg-dark-red text-center text-white">
           Add Bike Types
           <span className="block text-sm text-center">
             Add new bike type in the system
           </span>
         </div>
-        <AddBikeForm setAddCount={setAddCount} />
+        <AddBikeForm setAddCount={setAddCount} showAddBikes={showAddBikes} />
       </div>
-      <EditBikeForm addCount={addCount} />
+      <div
+        style={{display: showAddBikes ? "" : "none"}}
+        className={`${isAnimating ? 'shocked' : ''} flex justify-center w-1/2 mt-10 m-auto bg-white rounded-lg border border-gray-300 py-40 text-sm font-sm shadow-lg relative md:p-20`}>
+        <div className="absolute top-0  w-full py-4 bg-dark-red text-center text-white">
+          Edit Bike Types
+          <span className="block text-sm text-center">
+            Edit Bike Type
+          </span>
+        </div>
+        <EditBikeForm setAddCount={setAddCount} showInput={showInput} />
+      </div>
+      <ShowBikeTypes addCount={addCount} setIsAnimating={setIsAnimating} setShowInput={setShowInput} setShowAddBikes={setShowAddBikes} />
     </>
   );
 };
